ddf372e702324b9e857989dbaf97fa4a9be84c9b
Ryan Peden <ryan04@gmail.com>
2014-10-31 18:21:34 -0400

    added parser definitions

--- parsing.c
+++ parsing.c
@@ -5,6 +5,22 @@
 #include <editline/readline.h>
 
 int main(int argc, char** argv){
+	//Make some parsers
+	mpc_parser_t* Number 	= mpc_new("number");
+	mpc_parser_t* Operator 	= mpc_new("operator");
+	mpc_parser_t* Expr		= mpc_new("expr");
+	mpc_parser_t* RyLisp 	= mpc_new("rylisp");
+
+	//Define parsers
+	mpca_lang(MPCA_LANG_DEFAULT,
+		"							             \
+			number     : /-?[0-9]+/ ;            \
+			operator   : '+' | '-' | '*' | '/' ; \
+			expr       : <number> | '(' <operator> <expr>+ ')' ;  \
+			rylisp     : /^/ <operator> <expr>+ /$/ ;    \
+		",
+	Number, Operator, Expr, RyLisp);
+
 	puts("RyLisp Version 0.0.0.0.0.0.1");
 	puts("Press Ctrl-C to Exit\n");
 
@@ -22,5 +38,6 @@ int main(int argc, char** argv){
 		free(input);
 	}
 
+	mpc_cleanup(4, Number, Operator, Expr, RyLisp);
 	return 0;
 }
