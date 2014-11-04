ce445a66ed34bad9c3a9f47eeed9892ceb3d93d8
Ryan Peden <ryan04@gmail.com>
2014-11-03 20:44:39 -0500

    added built in eval function

--- parsing.c
+++ parsing.c
@@ -253,6 +253,18 @@ lval* builtin_list(lval* a){
 	return a;
 }
 
+lval* builtin_eval(lval* a){
+	LASSERT(a, a->count == 1, 
+		"Function 'eval' passed too many arguments");
+
+	LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+		"Function 'eval' passed incorrect type");
+
+	lval* x = lval_take(a, 0);
+	x->type = LVAL_SEXPR;
+	return lval_eval(x);
+}
+
 lval* lval_pop(lval* v, int i){
 	//find item at i
 	lval* x = v->cell[i];
