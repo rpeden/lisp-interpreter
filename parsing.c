2adaa18cb371a9071e8f633f915d6e2bb880bc7a
Ryan Peden <ryan04@gmail.com>
2014-11-05 13:51:50 -0500

    upgraded head function error reporting

--- parsing.c
+++ parsing.c
@@ -304,7 +304,9 @@ lval* lval_join(lval* x, lval* y){
 lval* builtin_head(lenv* e, lval* a){
 	//check error conditions
 	LASSERT(a, a->count == 1, 
-		"Function 'head' received too many arguments");
+		"Function 'head' received too many arguments. "
+		"Got %i, Expected %i.",
+		a->count, 1);
 
 	LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
 		"Function 'head' passed incorrect types");
