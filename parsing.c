101b3bcafbfcc36acd253933ab2de28973fedc93
Ryan Peden <ryan04@gmail.com>
2014-11-05 13:23:24 -0500

    added builtin_def to environment

--- parsing.c
+++ parsing.c
@@ -414,6 +414,7 @@ void lenv_add_builtins(lenv* e){
 	lenv_add_builtin(e, "tail", builtin_tail);
 	lenv_add_builtin(e, "eval", builtin_eval);
 	lenv_add_builtin(e, "join", builtin_join);
+	lenv_add_builtin(e, "def" , builtin_def);
 
 	//math functions
 	lenv_add_builtin(e, "+", builtin_add);
