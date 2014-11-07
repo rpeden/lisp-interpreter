640ded1977c55d50089c3de1591db0bb40f1d2d0
Ryan Peden <ryan04@gmail.com>
2014-11-07 06:49:01 -0500

    finished built in var function and removed old built in def function

--- parsing.c
+++ parsing.c
@@ -441,32 +441,6 @@ lval* builtin_div(lenv* e, lval* a){
 	return builtin_op(e, a, "/");
 }
 
-lval* builtin_def(lenv* e, lval* a){
-	LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
-		"Function 'def' passed incorrect type");
-
-	//first argument is symbol list
-	lval* syms = a->cell[0];
-
-	//ensure all elements of first list are symbols
-	for(int i = 0; i < syms->count; i++){
-		LASSERT(a, syms->cell[i]->type == LVAL_SYM,
-			"Function 'def' cannot define non symbol");
-	}
-
-	//check correct number of symbols and values
-	LASSERT(a, syms->count == a->count-1,
-		"Function 'def' cannot define incorrect number of values to symbols");
-
-	//assign copies of values to symbols
-	for(int i = 0; i < syms->count; i++){
-		lenv_put(e, syms->cell[i], a->cell[i+1]);
-	}
-
-	lval_delete(a);
-	return lval_sexpr();
-}
-
 lval* builtin_lambda(lenv* e, lval* a){
 	//check two arguments, each of which are q-expressions
 	LASSERT_NUM("\\", a, 2);
@@ -500,6 +474,31 @@ lval* builtin_var(lenv* e, lval* a, char* func){
 	LASSERT_TYPE(func, a, 0, LVAL_QEXPR);
 
 	lval* syms = a->cell[0];
+	for(int i = 0; i < syms->count; i++){
+		LASSERT(a, (syms->cell[i]->type == LVAL_SYM),
+			"Function '%s' cannot define non-symbol. "
+			"Got %s, Expected %s", func,
+			ltype_name(syms->cell[i]->type),
+			ltype_name(LVAL_SYM));
+	}
+
+	LASSERT(a, (syms->count == a->count-1),
+		"Function '%s' passed too many arguments for symbols. "
+		"Got %i, Expected %i.", func, syms->count, a->count-1);
+
+	for(int i=0; i < syms->count; i++){
+		//if 'def' define globally, if 'put' define locally
+		if(strcmp(func, "def") == 0){
+			lenv_def(e, syms->cell[i], a->cell[i+1]);
+		}
+
+		if(strcmp(func, "=") == 0){
+			lenv_put(e, syms->cell[i], a->cell[i+1]);
+		}
+	}
+
+	lval_delete(a);
+	return lval_sexpr();
 }
 
 void lenv_add_builtin(lenv* e, char* name, lbuiltin func){
