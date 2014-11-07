7ee53f03c9ad6319b5e3908b21e994169681c990
Ryan Peden <ryan04@gmail.com>
2014-11-06 19:28:40 -0500

    added assertion to ensure function arguments are not empty

--- parsing.c
+++ parsing.c
@@ -18,13 +18,18 @@
     "Function '%s' passed incorrect type for argument %i. " \
     "Got %s, Expected %s.", \
     func, index, ltype_name(args->cell[index]->type), ltype_name(expect))
-  
+
 //macro to assert number of arguments to function
 #define LASSERT_NUM(func, args, num) \
   LASSERT(args, args->count == num, \
     "Function '%s' passed incorrect number of arguments. " \
     "Got %i, Expected %i.", \
     func, args->count, num)
+
+//macro to ensure function arguments not empty
+#define LASSERT_NOT_EMPTY(func, args, index) \
+  LASSERT(args, args->cell[index]->count != 0, \
+    "Function '%s' passed {} for argument %i.", func, index);
 
 //Forward declarations
 struct lval;
